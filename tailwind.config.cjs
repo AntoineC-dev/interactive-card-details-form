9097c75b27edd70c6a6cff33cf76164bb5e2fa7a
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-06 00:18:29 -0400

    Add custom styles from FEM style-guide

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
+  colors: {
+    transparent: "transparent",
+    current: "currentColor",
+    primary: {
+      purple: "hsl(278, 94%, 30%)",
+      blue: "hsl(249, 99%, 64%)",
+      red: "hsl(0, 100%, 66%)",
+    },
+    neutral: {
+      white: "hsl(0, 0%, 100%)",
+      "light-gray": "hsl(270, 3%, 87%)",
+      "dark-gray": "hsl(279, 6%, 55%)",
+      purple: "hsl(278, 68%, 11%)",
+    },
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Space\\ Grotesk", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
