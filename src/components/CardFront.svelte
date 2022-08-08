ea085c288eed0eec00407d800375e8fd8d21c7e5
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-08 15:25:51 -0400

    Use starterfiles svg for card logo

--- src/components/CardFront.svelte
+++ src/components/CardFront.svelte
@@ -4,14 +4,7 @@
 
 <div class="relative max-w-max w-full">
   <img src="/bg-card-front.png" alt="" class="w-full max-w-[447px] h-auto" />
-  <div class="absolute top-[10%] left-[6%] flex items-center gap-3">
-    <!-- Circle fill -->
-    <div class="w-[clamp(1.50rem,calc(0.43rem+5.33vw),2.00rem)] aspect-square rounded-full bg-neutral-white" />
-    <!-- Circle border -->
-    <div
-      class="w-[clamp(0.75rem,calc(0.22rem+2.67vw),1.00rem)] aspect-square rounded-full border-2 border-neutral-white"
-    />
-  </div>
+  <img src="/card-logo.svg" alt="" class="absolute top-[10%] left-[6%] w-12 lg:w-20" />
   <!-- Card number: 0000 0000 0000 0000 -->
   <span
     class="absolute bottom-[30%] left-[6%] text-neutral-white text-[clamp(1.00rem,calc(-0.07rem+5.33vw),1.50rem)] lg:text-3xl tracking-widest"
